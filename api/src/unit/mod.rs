d049b4337addac3139dbe614258c4d4040eeaa9d
Steve Loveless <steve@agrian.com>
2018-12-17 15:07:27 -0800

    NO-TICKET cargo fmt for Rust 1.31 release

--- api/src/unit/mod.rs
+++ api/src/unit/mod.rs
@@ -194,7 +194,8 @@ mod tests {
                     "exponent": -4,
                     "annotation": null
                 }]
-        }"#.replace("\n", "")
+        }"#
+            .replace("\n", "")
             .replace(" ", "");
 
             let term1 =
