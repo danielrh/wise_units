fe0aa0daae6b7006a161cff37bc8a3f8ae3d0898
Joel Anna <joel@agrian.com>
2020-08-18 11:06:51 -0700

    DEV-12556: Add unit_expression_buf / unit_composition_buf to alllow for caller allocated string buffers

--- ffi/src/lib.rs
+++ ffi/src/lib.rs
@@ -19,6 +19,8 @@
 // C bindings don't include the module name, so ffi functions will need it.
 #![allow(clippy::module_name_repetitions)]
 
+use std::os::raw::c_char;
+use std::ptr;
 pub mod measurement;
 pub mod unit;
 
@@ -27,4 +29,37 @@ pub use wise_units::{Measurement, Unit};
 fn set_error_and_return<T>(message: String) -> *const T {
     ffi_common::error::set_last_err_msg(&message);
     std::ptr::null()
+}
+
+unsafe fn return_string_in_buffer(string: String, buffer: *mut c_char, length: i32) -> i32 {
+    if buffer.is_null() {
+        ffi_common::error::set_last_err_msg(
+            "Null pointer passed into return_string_in_buffer() as the buffer",
+        );
+        return -1;
+    }
+
+    let buffer = std::slice::from_raw_parts_mut(buffer as *mut u8, length as usize);
+
+    if string.len() >= buffer.len() {
+        ffi_common::error::set_last_err_msg(
+            format!(
+                "Buffer provided for writing the message is too small.
+            Expected at least {} bytes but got {}",
+                string.len() + 1,
+                buffer.len()
+            )
+            .as_str(),
+        );
+        buffer[0] = 0;
+        return (string.len() + 1) as i32;
+    }
+
+    ptr::copy_nonoverlapping(string.as_ptr(), buffer.as_mut_ptr(), string.len());
+
+    // Add a trailing null so people using the string as a `char *` don't
+    // accidentally read into garbage.
+    buffer[string.len()] = 0;
+
+    return string.len() as i32;
 }
